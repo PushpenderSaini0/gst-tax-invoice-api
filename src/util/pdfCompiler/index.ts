aa25a0c31adc1dd32d89c4b19816998f65c7e9ab
PushpenderSaini0 <54404738+PushpenderSaini0@users.noreply.github.com>
2021-07-11 15:32:23 +0530

    Added basic table render

--- src/util/pdfCompiler/index.ts
+++ src/util/pdfCompiler/index.ts
@@ -1,7 +1,25 @@
 import { Request, Response } from 'express';
 
+const sampleData = [
+    {
+        name: 'asdf',
+        qt: '1',
+        price: '100',
+    },
+    {
+        name: 'bsdf',
+        qt: '12',
+        price: '1200',
+    },
+    {
+        name: 'csdf',
+        qt: '13',
+        price: '3100',
+    }
+];
+
 const pdfCompiler = (req: Request, res: Response): string => {
-    res.render('basic', { title: 'Hey', message: 'Hello there!' });
+    res.render('basic', { items: sampleData });
     return req.body;
 };
 
